c5d784c7612a259f072793c84b297e15f3c91e28
Pip Bbaale <namugera.philip@gmail.com>
2021-01-28 10:24:38 +0300

    adding classmethod function to check if list name exists

--- models.py
+++ models.py
@@ -7,3 +7,10 @@ class Airflow:
     def save_todolist_item(self):
         Airflow.todolist_list.append(self)
 
+    @classmethod
+    def todolist_exists(cls, todolist_name):
+        for list_name in cls.todolist_list:
+            if list_name.todolist_name == todolist_name:
+                return True
+            return False
+
